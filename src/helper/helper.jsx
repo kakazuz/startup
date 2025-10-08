849da273ccc7c06f56ebfc385abcab621fb11f9e
kakazuz <zzenichi@byu.edu>
2025-10-07 21:28:20 -0600

    final chnages to everything

--- src/helper/helper.jsx
+++ src/helper/helper.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Helper() {
     return (
-      <main className="container-fluid bg-secondary text-center p-5">
+      <main className=" text-center p-5">
         <div> Helper Coming soon!</div>  
       </main>
     );
